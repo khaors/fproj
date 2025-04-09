73e4345d01c09fd2db37d46e62f0d71637c730be
khaors <khaors@gmail.com>
2025-04-09 06:20:33 -0500

    app folder added to repository

---
+++ app/fproj.f90
@@ -0,0 +1,11 @@
+! Command line options 
+! http://proj.maptools.org/man_proj.html
+program fproj
+    use fproj_utilities, only: pj;
+    use M_CLI2,  only : set_args, get_args, set_mode;
+    implicit none
+    !
+    type(pj) :: pj_obj
+
+  !call say_hello()
+end program fproj
