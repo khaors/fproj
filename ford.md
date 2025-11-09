be871bd72f310ab71a6878d8fa3ee75617c7dc54
khaors <khaors@gmail.com>
2025-11-08 19:37:13 -0500

    Doc and README updated

--- ford.md
+++ ford.md
@@ -1,6 +1,6 @@
 ---
-project: 
-summary: transforms geospatial coordinates from one coordinate reference system (CRS) to another. This includes cartographic projections as well as geodetic transformations. PROJ is released under the X/MIT open source license
+project: fproj
+summary: *fproj* is a modern Fortran interface to the _proj_ library. Proj transforms geospatial coordinates from one coordinate reference system (CRS) to another. This includes cartographic projections as well as geodetic transformations. PROJ is released under the X/MIT open source license
 author: Oscar Garcia-Cabrejo
 email: khaors@gmail.com
 github: https://github.com/khaors/fproj
@@ -21,5 +21,60 @@ md_extensions: markdown.extensions.toc
                markdown.extensions.smarty
 source: true
 ---
-{!README.md!}
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Fortran](https://img.shields.io/badge/Fortran-734F96?logo=fortran&logoColor=fff)](https://fortran-lang.org/)
+[![fpm](https://img.shields.io/badge/fpm-Fortran_package_manager-734f96)](https://fpm.fortran-lang.org)
+
+Proj is a library for working with coordinate transformations and geographical projections in spatial problems.  This is a modern fortran interface that only includes the common procedures of the proj library.
+
+## Motivation
+
+## Get the code
+
+Clone the repository
+
+```
+git clone https://github.com/khaors/fproj
+cd fproj
+```
+
+## Compilation with fpm 
+
+The Fortran Package Manager (fpm) can be used to build `fproj` using the provided `fpm.toml` file:
+
+
+```
+fpm build --profile release
+```
+
+## Testing
+
+The test can be executed using:
+
+```
+fpm test --profile release
+```
+
+## Documentation
+
+The documentation of the library can be accessed in the following links:
+
+- [html files](https://khaors.github.io/fproj)
+
+## Licensing, Authors, Acknowledgements
+_fproj_ was written by Oscar Garcia-Cabrejo and is distributed under the [MIT license](https://github.com/khaors/fproj/blob/master/LICENSE). 
+
+You can cite _fproj_ in research publications and reports as follows:
+* Garcia-Cabrejo, O. (2025). ***fproj: A modern Fortran port of the proj library***. https://github.com/khaors/fproj. Accessed: *day month year*.
+
+BibTeX entry:
+```
+@misc{Garcia-Cabrejo25,
+ author = {Garcia-Cabrejo, O},
+ title 	= {{fproj: A modern Fortran port of the proj library}},
+ year 	= 2025,
+ howpublished = {\url{https://github.com/khaors/fproj}},
+ note 	= {Accessed: day month year}
+}
+```
 ---
--- src/CRS_utilities.f90
+++ src/CRS_utilities.f90
@@ -1,4 +1,11 @@
+!! author: Oscar Garcia-Cabrejo
+!! date: 11/08/2025
+!! version: 0.1
+!!  Define a class to represent the CRS (Coordinate Reference System)
+!!
 module CRS_utilities
+!! The CRS class is used to encapsulate the information that defines a 
+!! Coordinate Reference System
     use iso_fortran_env, only: wp => real64;
     use,intrinsic :: iso_c_binding;
     use fproj_utilities, only: pj,pj_context,proj_context_create,proj_context_destroy,&
@@ -12,6 +19,8 @@ module CRS_utilities
     use stdlib_error, only: check,error_stop;
     !
     type CRS
+!! This class encapsulates the information used to define a Coordinate
+!! Reference System CRS
         private
             character(len=NUMCHAR) :: epsg_string 
             integer :: epsg_code
@@ -31,9 +40,11 @@ module CRS_utilities
 !========================================================================================
         subroutine create_from_epsg_number(my_crs,epsg_code)
 !========================================================================================
+!! Constructor of the CRS class form the EPSG code
             class(CRS) :: my_crs
+!! A `CRS` object
             integer :: epsg_code 
-            !
+!! An integer variable with the EPSG code
             character(len=8) :: current_epsg_number
 ! Check if epsg_code starts with "EPSG:"
             my_crs%epsg_code=epsg_code;
@@ -48,9 +59,11 @@ module CRS_utilities
 !========================================================================================
         subroutine create_from_epsg_string(my_crs,epsg_string)
 !========================================================================================
+!! Constructor of the CRS class form the EPSG string
             class(CRS) :: my_crs
+!! A `CRS` object
             character(len=*) :: epsg_string
-        !
+!! A character variable with the EPSG string
             if(starts_with(trim(epsg_string),"EPSG:")) then
                 my_crs%epsg_string=trim(epsg_string);
                 my_crs%pj_obj=proj_create(pj_context_default,trim(my_crs%epsg_string)//&
@@ -63,9 +76,11 @@ module CRS_utilities
 !========================================================================================
         subroutine create_from_proj4(my_crs,proj_string)
 !========================================================================================
+!! Constructor of the CRS class form the proj4 string
             class(CRS) :: my_crs
+!! A `CRS` object
             character(len=*) :: proj_string
-!
+!! A character variable with the proj4 string
             my_crs%proj_string=trim(proj_string);
             my_crs%pj_obj=proj_create(pj_context_default,trim(my_crs%proj_string)//&
                 c_null_char);
@@ -74,8 +89,9 @@ module CRS_utilities
 !========================================================================================
         subroutine destroy_crs(my_crs)
 !========================================================================================
+!! Destructor of the CRS class
             class(CRS) :: my_crs
-!
+!! A `CRS` object
             my_crs%pj_obj=proj_destroy(my_crs%pj_obj);
 !
         end subroutine destroy_crs
@@ -83,13 +99,16 @@ module CRS_utilities
 !========================================================================================
         subroutine write_crs(my_crs,unit_,fmt)
 !========================================================================================
+!! Subroutine to write the information of a CRS object
             class(CRS) :: my_crs
+!! A `CRS` object                
             integer :: unit_
+!! An integer variable with the output unit
             character(len=*) :: fmt 
-!
+!! A character variable with the format to print the information 
+!! of the CRS object
             call print_info(proj_pj_info(my_crs%pj_obj));
 !
         end subroutine write_crs
-
-    
+!
 end module CRS_utilities
